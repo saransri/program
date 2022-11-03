61fd0456cbd234ff08146c47b9bf3ab132057259
saransri <saravanan120894@gmail.com>
2022-11-03 10:38:13 +0530

    message

---
+++ src/acessmodifier/bikedetails.java
@@ -0,0 +1,15 @@
+package acessmodifier;
+
+public   class bikedetails {
+	public static void  hari(){
+		
+	
+		System.out.println("hari");
+	}
+public static void sakthi(){
+	System.out.println("sakthi");
+}
+public static void main(String[] args) {
+	
+}
+}
---
+++ src/acessmodifier/variousmodels.java
@@ -0,0 +1,9 @@
+package acessmodifier;
+
+public class variousmodels extends bikedetails{
+
+public static void main(String[] args) {
+	bikedetails.hari();
+}
+}
+
